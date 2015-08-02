b7191872707092003bff9a5610997e15c7e23c7d
NickTroutwine <Nat88120@ucmo.edu>
2015-08-02 14:28:16 -0700

    server is not working

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
-var http = require('http');
-var fs = require('fs');
-fs.readFile('./client/pages/application.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.write(html);  
-        response.end();  
-    }).listen(3000);
+// var http = require('http');
+// var fs = require('fs');
+// fs.readFile('./client/pages/index.html', function (err, html) {
+//     if (err) {
+//         throw err; 
+//     }       
+//     http.createServer(function(request, response) {  
+//         response.write(html);  
+//         response.end();  
+//     }).listen(3000);
+// });
+var express = require('express');
+var app = express(); 
+
+app.all('/',function(req,res,next){
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	next();
 });
+app.use(express.static('client'));
+app.listen(3000);
+
+module.exports = app;
