7f0f2a9c13a6b1fdc39fe2a1ea9aa3bf24692f69
NickTroutwine <Nat88120@ucmo.edu>
2015-08-02 15:25:39 -0700

    Server is running angular worked, best feeling of my whole life

---
+++ client/main.js
@@ -0,0 +1,23 @@
+(function() {
+angular.module('main', [
+  'ui.router'
+])
+
+.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'pages/home.html'
+    })
+    .state('services', {
+      url: '/services',
+      templateUrl: 'pages/services.html'
+    })
+    .state('application', {
+      url: '/application',
+      templateUrl: 'pages/application.html'
+    })
+
+    $urlRouterProvider.otherwise('/');
+});
+})();
