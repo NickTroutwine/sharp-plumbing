8005330f25c0993c69daca0a71b877f237f23bec
MattEubanks <moosefan94@gmail.com>
2015-08-03 15:58:31 -0700

    cleaned up pages and index code

--- server/server.js
+++ server/server.js
@@ -13,10 +13,11 @@ var express = require('express');
 var app = express(); 
 
 app.all('/',function(req,res,next){
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
-	next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
 });
+
 app.use(express.static('client'));
 app.listen(3000);
 
