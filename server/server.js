4eb7188e8872ef415ba399caf8a5f768197a0336
MattEubanks <moosefan94@gmail.com>
2015-08-04 09:05:27 -0700

    added procfile

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@
 // });
 var express = require('express');
 var app = express(); 
+var path = require('path');
 
 app.all('/',function(req,res,next){
   res.header("Access-Control-Allow-Origin", "*");
@@ -18,7 +19,20 @@ app.all('/',function(req,res,next){
   next();
 });
 
+app.get('/services', function (req, res){
+  res.sendFile(path.join(__dirname, '/../client/index.html'));
+});
+app.get('/application', function (req, res){
+  res.sendFile(path.join(__dirname, '/../client/index.html'));
+});
+
 app.use(express.static('client'));
-app.listen(3000);
+if('PORT' in process.env){
+	console.log('port exist?');
+	app.listen(parseInt(process.env.PORT, 10));
+} else{
+	app.listen(3000);
+}
+
 
 module.exports = app;
